12c15a1563c9eabcc33e75529d2535bba1bd44f8
checkr <ugochiori@gmail.com>
2025-02-05 09:15:55 +0100

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import MealPlan from "./components/MealPlan"
-import Footer from "./Footer"
-import { Analytics } from "@vercel/analytics/react"
+import MealPlan from "./components/MealPlan";
+import MealPlanThree from "./components/MealPlanThree";
+import MealPlanTwo from "./components/MealPlanTwo";
+import MealPlanFour from "./components/MealPlanFour" ;
+import Footer from "./Footer";
+import { Analytics } from "@vercel/analytics/react";
+import {Routes, Route} from "react-router-dom";
+import NavBar from "./navigation/NavBar";
 
 function App() {
  
 
   return (
     <div className="border border-black p-1 bg-teal-200">
-   <MealPlan />
+      <NavBar/>
+    <Routes>
+<Route path="/" element={<MealPlan/>} />
+<Route path="/mealplantwo" element={<MealPlanTwo/>} />
+<Route path="/mealplanthree" element={<MealPlanThree/>} />
+<Route path="/mealplanfour" element={<MealPlanFour/>} />
+ 
+
+   </Routes>
    <Footer />
    <Analytics/>
     </div>
